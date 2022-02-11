4ec0aab01c8cd793b68b00fa91b15011f2c4a096
botac135 <86068776+botac135@users.noreply.github.com>
2022-02-11 12:45:36 +0530

    Update commands.py

--- LuciferMoringstar_Robot/commands.py
+++ LuciferMoringstar_Robot/commands.py
@@ -15,8 +15,7 @@ async def start_message(bot, message):
     if len(message.command) != 2:
         if message.from_user.id not in ADMINS: 
             buttons = [[
-             InlineKeyboardButton('🔍𝐒𝐄𝐀𝐑𝐂𝐇 𝐅𝐈𝐋𝐄', switch_inline_query_current_chat=qurey)
-             ],[
+             InlineKeyboardButton('🔍𝐒𝐄𝐀𝐑𝐂𝐇 𝐅𝐈𝐋𝐄', switch_inline_query_current_chat=qurey)],[
              InlineKeyboardButton("🧐 𝐇𝐄𝐋𝐏 🍿", callback_data="help"),
              InlineKeyboardButton("🧐 𝐀𝐁𝐎𝐔𝐓 🍿", callback_data="about") 
              ],[
@@ -25,6 +24,7 @@ async def start_message(bot, message):
              ]]
         else:
             buttons = [[
+             InlineKeyboardButton('🔍𝐒𝐄𝐀𝐑𝐂𝐇 𝐅𝐈𝐋𝐄', switch_inline_query_current_chat=query)],[
              InlineKeyboardButton("🧐 𝐇𝐄𝐋𝐏 🍿", callback_data="bot_owner"),
              InlineKeyboardButton("🧐 𝐀𝐁𝐎𝐔𝐓 🍿", callback_data="about") 
              ],[
