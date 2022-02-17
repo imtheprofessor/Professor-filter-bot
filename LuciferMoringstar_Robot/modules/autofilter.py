02f0e8b7eebbe4d187026c212d7b518a787ad588
botac135 <86068776+botac135@users.noreply.github.com>
2022-02-17 08:41:15 +0530

    Update autofilter.py

--- LuciferMoringstar_Robot/modules/autofilter.py
+++ LuciferMoringstar_Robot/modules/autofilter.py
@@ -21,7 +21,9 @@ async def group_filters(client, message):
                 filename = f"{file.file_name}"
                 filesize = f"{get_size(file.file_size)}"
                 btn.append(
-                    [InlineKeyboardButton(text=f"{filename}", callback_data=f"lucifermoringstar_robot#{file_id}"),
+                    [InlineKeyboardButton("🎥 𝙈𝙤𝙫𝙞𝙚𝙨", url=f"http://t.me/freakersmovies)
+                    ],[
+                     InlineKeyboardButton(text=f"{filename}", callback_data=f"lucifermoringstar_robot#{file_id}"),
                      InlineKeyboardButton(text=f"{filesize}", callback_data=f"lucifermoringstar_robot#{file_id}")]
                 )
         else:
